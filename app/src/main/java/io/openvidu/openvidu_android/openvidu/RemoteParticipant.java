42dc5098d857f8209bef382d87432c085860a53a
m01i0ng <alley.ma@qq.com>
2021-12-13 16:56:42 +0800

    chore: tidy

--- app/src/main/java/io/openvidu/openvidu_android/openvidu/RemoteParticipant.java
+++ app/src/main/java/io/openvidu/openvidu_android/openvidu/RemoteParticipant.java
@@ -2,46 +2,45 @@ package io.openvidu.openvidu_android.openvidu;
 
 import android.view.View;
 import android.widget.TextView;
-
 import org.webrtc.SurfaceViewRenderer;
 
 public class RemoteParticipant extends Participant {
 
-    private View view;
-    private SurfaceViewRenderer videoView;
-    private TextView participantNameText;
+  private View view;
+  private SurfaceViewRenderer videoView;
+  private TextView participantNameText;
 
-    public RemoteParticipant(String connectionId, String participantName, Session session) {
-        super(connectionId, participantName, session);
-        this.session.addRemoteParticipant(this);
-    }
+  public RemoteParticipant(String connectionId, String participantName, Session session) {
+    super(connectionId, participantName, session);
+    this.session.addRemoteParticipant(this);
+  }
 
-    public View getView() {
-        return this.view;
-    }
+  public View getView() {
+    return this.view;
+  }
 
-    public void setView(View view) {
-        this.view = view;
-    }
+  public void setView(View view) {
+    this.view = view;
+  }
 
-    public SurfaceViewRenderer getVideoView() {
-        return this.videoView;
-    }
+  public SurfaceViewRenderer getVideoView() {
+    return this.videoView;
+  }
 
-    public void setVideoView(SurfaceViewRenderer videoView) {
-        this.videoView = videoView;
-    }
+  public void setVideoView(SurfaceViewRenderer videoView) {
+    this.videoView = videoView;
+  }
 
-    public TextView getParticipantNameText() {
-        return this.participantNameText;
-    }
+  public TextView getParticipantNameText() {
+    return this.participantNameText;
+  }
 
-    public void setParticipantNameText(TextView participantNameText) {
-        this.participantNameText = participantNameText;
-    }
+  public void setParticipantNameText(TextView participantNameText) {
+    this.participantNameText = participantNameText;
+  }
 
-    @Override
-    public void dispose() {
-        super.dispose();
-    }
+  @Override
+  public void dispose() {
+    super.dispose();
+  }
 }
